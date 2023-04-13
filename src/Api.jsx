bc6675f5c24068a6066765f812acb7e2eb731aa1
joaolvp <joaolvpj@gmail.com>
2023-04-13 13:04:52 -0400

    Criando o projeto e as chamadas de função da api

---
+++ src/Api.jsx
@@ -0,0 +1,28 @@
+import axios from "axios";
+
+const instance = axios.create({
+    baseURL: 'https://pokeapi.co/api/v2/',
+});
+
+const api = {
+    getType: async () => {
+        try{
+            const res = await instance.get(`/type`);
+            return res.data;
+        }catch(error){
+            console.log(error.message);
+            return undefined;
+        }
+    },
+    GetPokemon: async(type_id) => {
+        try{
+            const res = await instance.get(`/type/${type_id}`);
+            return res.data;
+        }catch(error){
+            console.log(error.message);
+            return undefined;
+        }
+    },
+}
+
+export default api;
